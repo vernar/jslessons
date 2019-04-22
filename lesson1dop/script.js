0aa2828c5a0c3d5cc58079f30d5b6220bcab92a4
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-04-23 01:32:18 +0300

    fix some issues

--- lesson1dop/script.js
+++ lesson1dop/script.js
@@ -1,17 +1,29 @@
 'use strict';
 
-//1
-let num = '33721';
+/*
+1) Создать переменную num со значением 33721
+Вывести в консоль произведение (умножение) цифр этого числа
+ */
+let num = 33721;
 
-let summ = 0;
-for(let i = 0; i < num.length; i++ ){
-    summ += Number(num[i]);
-}
+let summ = num.toString().split("").reduce(function (mult, current) {
+    return mult * current;
+});
 
 console.log(summ);
 
 
-//2
+/*
+2) Полученный результат возвести в степень 3, используя только 1 оператор
+(Math.pow не подходит)
+Вывести на экран первые 2 цифры полученного числа
+ */
 
 let numPow = String(summ ** 3);
-console.log(numPow[0] +  numPow[1]);
+let result = numPow.toString().split("").reduce(function (mult, current, i) {
+    return i < 2 ? mult += +current : mult;
+});
+
+console.log(result);
+
+//console.log(numPow[0] +  numPow[1]);
