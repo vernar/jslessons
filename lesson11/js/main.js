5e8180fb2820899cc7f6c1ba7ec0110c8d501b45
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-05-16 01:24:22 +0300

    add phone checker for phone field

--- lesson11/js/main.js
+++ lesson11/js/main.js
@@ -1,5 +1,91 @@
 'use strict';
 
+String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+};
+
+class PhoneTemplate {
+    constructor(element, template, startPos) {
+        this.phoneInput = element;
+        this.template = template;
+        this.curentString = template;
+        this.keyCode = '';
+
+        this.phoneInput.value = template;
+        this.setCursorPosition(startPos);
+
+        this.startObservers();
+    }
+
+    startObservers() {
+        document.addEventListener('keydown', (event) => {
+            this.keyCode = event.key;
+        });
+
+        document.addEventListener('input', (event) => {
+            if (this.serviceKey() === true) {
+                return;
+            }
+            let inputValue = this.phoneInput.value,
+                currentPosition = this.phoneInput.selectionStart,
+                inputPosition = this.phoneInput.selectionStart - 1;
+
+            while (this.template[inputPosition] !== '_' && inputPosition < this.template.length) {
+                inputPosition++;
+                this.setCursorPosition(inputPosition);
+            }
+
+            if (!isNaN(parseInt(inputValue[inputPosition])) && this.template[inputPosition] === '_') {
+                this.curentString = this.curentString.replaceAt(inputPosition, inputValue[inputPosition]);
+                this.setInput(this.curentString);
+                this.setCursorPosition(currentPosition);
+            } else {
+                this.setInput(this.curString);
+                this.setCursorPosition(inputPosition);
+            }
+
+            while (this.template[currentPosition] !== '_' && currentPosition < this.template.length) {
+                currentPosition++;
+                this.setCursorPosition(currentPosition);
+            }
+
+        });
+    }
+
+    setInput(text = '') {
+        this.curentString = text === '' ? this.curentString : text;
+        this.phoneInput.value = this.curentString;
+    }
+
+    setCursorPosition(pos) {
+        this.phoneInput.setSelectionRange(pos,pos);
+    }
+
+    serviceKey() {
+        let keyCode = this.keyCode,
+            currentPosition = this.phoneInput.selectionStart;
+
+        if (keyCode === 'ArrowLeft' || keyCode === 'ArrowRight') {
+            return true;
+        }
+
+        if (keyCode === 'Delete') {
+            return true;
+        }
+
+        if (keyCode === 'Backspace') {
+            while (this.template[currentPosition] !== '_' && currentPosition > 0) {
+                currentPosition--;
+            }
+            this.curentString = this.curentString.replaceAt(currentPosition, this.template[currentPosition]);
+            this.setInput();
+            this.setCursorPosition(currentPosition);
+            return true;
+        }
+        return false;
+    }
+}
+
 class Page {
     constructor(){
         this.initData();
@@ -34,7 +120,8 @@ class Page {
         //navigation menu
         this.navigationMenu = document.querySelector('nav ul');
 
-        //form
+        //form phone
+        this.phoneInput = document.querySelector('.popup-form .popup-form__input');
         this.form = document.querySelector('.main-form');
         this.input = this.form.getElementsByTagName('input');
         this.statusMessage = document.createElement('div');
@@ -46,6 +133,7 @@ class Page {
         this.initClock(this.deadline);
         this.initModal();
         this.initSoftScroll();
+        this.initPhoneChecker();
         this.initAjaxPhoneSend();
     }
 
@@ -156,6 +244,10 @@ class Page {
             }
 
         });
+    }
+
+    initPhoneChecker() {
+        let template = new PhoneTemplate(this.phoneInput, '+375 (__) ___-__-__', 6);
     }
 
     initAjaxPhoneSend() {
