09868555c11ed3a23397e52c3cc4a9d71980769d
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-05-19 15:30:36 +0300

    3) Исправить баги в логике калькулятора

--- lesson13/js/main.js
+++ lesson13/js/main.js
@@ -155,6 +155,7 @@ class Page {
         this.initAjaxPhoneSend();
         this.initAjaxContactsSend();
         this.initSlider(0);
+        this.initCalculator();
     }
 
     initTabSelector (initTabNumber) {
@@ -436,6 +437,59 @@ class Page {
             });
         });
     }
+
+    _calculate() {
+        let personsSum = +this.persons.value,
+            daysSum = +this.restDays.value,
+            place = +this.place.options[this.place.selectedIndex].value,
+            total = 0;
+
+        if (personsSum > 0 && daysSum > 0) {
+            total = (daysSum + personsSum) * place * 4000;
+        } else {
+            total = 0;
+        }
+
+        this.calcTotalValue.innerHTML = total.toString();
+    }
+
+    _checkValue(element, preValue) {
+        let inputValue = element.value,
+            inputPosition = element.selectionStart < 1 ? 0 : element.selectionStart-1;
+
+        if (isNaN(parseInt(inputValue[inputPosition]))){
+            element.value = preValue;
+            element.setSelectionRange(inputPosition,inputPosition);
+        }
+    }
+
+    initCalculator() {
+        this.persons = document.querySelectorAll('.counter-block-input')[0];
+        this.restDays = document.querySelectorAll('.counter-block-input')[1];
+        this.place = document.getElementById('select');
+        this.calcTotalValue = document.querySelector('#total');
+
+        let prevPersonsValue = '',
+            prevRestDaysValue = '';
+
+        this.calcTotalValue.innerHTML = '0';
+        this.persons.setAttribute('type', 'input');
+        this.restDays.setAttribute('type', 'input');
+
+        this.persons.addEventListener('input', (event) => {
+            this._checkValue(this.persons, prevPersonsValue);
+            this._calculate();
+            prevPersonsValue = this.persons.value;
+        });
+        this.restDays.addEventListener('input', () => {
+            this._checkValue(this.restDays, prevRestDaysValue);
+            this._calculate();
+            prevRestDaysValue = this.restDays.value;
+        });
+        this.place.addEventListener('change', () => this._calculate());
+
+    }
+
 }
 
 window.addEventListener('DOMContentLoaded', function () {
