e2f3244fc06a2f2a9345c0bd3525a255e43d0be9
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-05-19 20:35:44 +0300

    5) Написать функцию, что при клике на “Выбрать тур” , “Получить консультацию” или “Расписание туров” (все 3 элемента) подложка (класс overlay) медленно появлялась на странице (через прозрачность), а само модальное окно (класс modal) плавно выезжало сверху

--- lesson14/js/main.js
+++ lesson14/js/main.js
@@ -1,4 +1,25 @@
 'use strict';
 $(document).ready(() => {
+    function toggleModal() {
+        $('.overlay').fadeToggle('slow');
+        $('.modal')
+            .fadeToggle('slow')
+            .css({top:-800,position:'absolute'})
+            .animate({
+            top: 0,
+        }, 1000);
+    }
+
+
+    $('.list-item:first').hover(() => {
+        $(this).toggleClass('active');
+    });
+
+    $('.main_btna').on('click', () => toggleModal());
+    $('.main_btn').on('click', () => toggleModal());
+    $(".main_nav [href^='#sheldure']").on('click', () => toggleModal());
+
+    $('.overlay').on('click', () => toggleModal());
+
 
 });
