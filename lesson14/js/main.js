b035689e755f606c1aab9f6f1d04e60ce015bb45
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-05-19 20:55:13 +0300

    6) Написать функцию, что при клике на крестик всё происходило бы наоборот: подложка исчезала, модальное окно уезжало вверх

--- lesson14/js/main.js
+++ lesson14/js/main.js
@@ -1,15 +1,20 @@
 'use strict';
 $(document).ready(() => {
     function toggleModal() {
+        let yStartPos = -800,
+            yEndPos = 0;
+        if ($('.overlay').first().css('display') === "block") {
+            yStartPos = 0;
+            yEndPos = -800;
+        }
+
         $('.overlay').fadeToggle('slow');
         $('.modal')
-            .fadeToggle('slow')
-            .css({top:-800,position:'absolute'})
+            .css({display:'block',top: yStartPos,position:'absolute'})
             .animate({
-            top: 0,
+            top: yEndPos,
         }, 1000);
     }
-
 
     $('.list-item:first').hover(() => {
         $(this).toggleClass('active');
@@ -20,6 +25,5 @@ $(document).ready(() => {
     $(".main_nav [href^='#sheldure']").on('click', () => toggleModal());
 
     $('.overlay').on('click', () => toggleModal());
-
-
+    $('.close').on('click', () => toggleModal());
 });
