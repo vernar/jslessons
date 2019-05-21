2fafc677041f7dc8568c1a533a68c42f6994a49f
Dmitry Arbuzov <arbuzovdev@gmail.com>
2019-05-21 16:22:50 +0300

    fix input number to field

--- lesson13/js/main.js
+++ lesson13/js/main.js
@@ -453,41 +453,18 @@ class Page {
         this.calcTotalValue.innerHTML = total.toString();
     }
 
-    _checkValue(element, preValue) {
-        let inputValue = element.value,
-            inputPosition = element.selectionStart < 1 ? 0 : element.selectionStart-1;
-
-        if (isNaN(parseInt(inputValue[inputPosition]))){
-            element.value = preValue;
-            element.setSelectionRange(inputPosition,inputPosition);
-        }
-    }
-
     initCalculator() {
-        this.persons = document.querySelectorAll('.counter-block-input')[0];
-        this.restDays = document.querySelectorAll('.counter-block-input')[1];
-        this.place = document.getElementById('select');
-        this.calcTotalValue = document.querySelector('#total');
-
-        let prevPersonsValue = '',
-            prevRestDaysValue = '';
 
         this.calcTotalValue.innerHTML = '0';
-        this.persons.setAttribute('type', 'input');
-        this.restDays.setAttribute('type', 'input');
-
         this.persons.addEventListener('input', (event) => {
-            this._checkValue(this.persons, prevPersonsValue);
+            event.target.value = event.target.value.replace(/[^\d]/g, '');
             this._calculate();
-            prevPersonsValue = this.persons.value;
         });
-        this.restDays.addEventListener('input', () => {
-            this._checkValue(this.restDays, prevRestDaysValue);
+        this.restDays.addEventListener('input', (event) => {
+            event.target.value = event.target.value.replace(/[^\d]/g, '');
             this._calculate();
-            prevRestDaysValue = this.restDays.value;
         });
         this.place.addEventListener('change', () => this._calculate());
-
     }
 
 }
